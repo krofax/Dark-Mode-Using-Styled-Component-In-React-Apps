6496d51c308e023729d14afe5c1e5c698e107dd5
Blessing Krofegha <bkrofegha@gmail.com>
2020-04-06 12:21:19 +0200

    increase button text

--- src/components/theme.js
+++ src/components/theme.js
@@ -2,12 +2,12 @@ export const lightTheme = {
     body: '#FFF',
     text: '#363537',
     toggleBorder: '#FFF',
-    gradient: 'linear-gradient(#39598A, #79D7ED)',
+    gradient: '#363537',
 }
 
 export const darkTheme = {
-    body: '#000',
+    body: '#363537',
     text: '#FAFAFA',
     toggleBorder: '#6B8096',
-    gradient: 'linear-gradient(#091236, #1E215D)',
+    gradient: '#999',
 }
